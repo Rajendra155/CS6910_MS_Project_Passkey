44a0c9c44301ee0f005951f44950822197af578f
Rajendra155 <guttab@bgsu.edu>
2025-03-09 15:03:48 -0400

    Updated passkey.js for frontend

--- src/component/passkey.js
+++ src/component/passkey.js
@@ -37,7 +37,7 @@ function Passkey() {
 
     setError('');
     try {
-      const { data: publicKeyCredentialCreationOptions } = await axios.post('https://cs6910-ms-project-passkey-2.onrender.com/webauthn/register', { email });
+      const { data: publicKeyCredentialCreationOptions } = await axios.post('webauthn/register', { email });
 
       const publicKeyCredentialCreationOptionsParsed = {
         // Convert base64url challenge to Uint8Array
@@ -58,7 +58,7 @@ function Passkey() {
         publicKey: publicKeyCredentialCreationOptionsParsed,
       });
 
-      await axios.post('https://cs6910-ms-project-passkey-2.onrender.com/webauthn/register/complete', {
+      await axios.post('webauthn/register/complete', {
         email,
         credential,
       });
@@ -80,7 +80,7 @@ function Passkey() {
 
     try {
       const { data: publicKeyCredentialRequestOptions } = await axios.post(
-        'https://cs6910-ms-project-passkey-2.onrender.com/webauthn/authenticate',
+        'webauthn/authenticate',
         { email }
       );
 
@@ -114,7 +114,7 @@ function Passkey() {
         }
       };
 
-      await axios.post('https://cs6910-ms-project-passkey-2.onrender.com/webauthn/authenticate/complete', {
+      await axios.post('webauthn/authenticate/complete', {
         email,
         assertion: assertionResponse
       });
